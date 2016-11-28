f794d0bba2f713605ae55f5317ef8965d6bfd98b
zain18-meet <zainq2013@icliod.com>
2016-11-28 19:48:10 +0200

    cool

--- UserAccount.py
+++ UserAccount.py
@@ -11,6 +11,8 @@ class UserAccount :
         :param secret: user's secret, accessible only with password (string)
         """
         self.username=username
+        self.password=password
+        self.secret=secret
         #Complete the __init__ method
 
     def print_secret(self,password_attempt):
@@ -23,3 +25,10 @@ class UserAccount :
         #Complete this method - check the input variable, password_attempt,
         #against the password for this user (the value of the instance variable, password).
         #If, and only if, there's a match, then print the user's secret to the screen.
+
+        if  password_attempt == self.password:
+            print(str(self.secret))
+
+            # wrote 'str' incase secret is an integer
+        else:
+            print('incorrect')
